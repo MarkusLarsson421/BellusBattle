1c073b290b6b19f9a4313706c1ca45f001003c51
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-10-10 20:20:39 +0200

    comment in Aim.cs

--- Assets/Scripts/Aim/Aim.cs
+++ Assets/Scripts/Aim/Aim.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Videon som jag har följt
+// https://www.youtube.com/watch?v=gaDFNCRQr38&t=23s
 public class Aim : MonoBehaviour
 {
     public Transform arm;
