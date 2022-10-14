6e9b1a931d4640ee05aa1e63d13631fba011bd56
khalosh74 <alrassk74@gmail.com>
2022-10-14 13:16:46 +0200

    dash fix

--- Assets/Scripts/Dash/FinalDash.cs
+++ Assets/Scripts/Dash/FinalDash.cs
@@ -6,31 +6,28 @@ public class FinalDash : MonoBehaviour
     [SerializeField] private bool canDash = true;
     [SerializeField] private bool stopGravityWhileDashing = true;
     private bool isDashing;
+    private bool isFacingRight = true;
     [SerializeField] private float dashingDistace = 24f;
     [SerializeField] private float dashingDuration = 0.2f;
     [SerializeField] private float dashingActivationCooldown = 0.01f;
+    //[SerializeField] private TrailRenderer tr; // these variable makes visual effect
 
     private Vector3 velocity;
     private PlayerMovement movement;
-    //[SerializeField] private TrailRenderer tr; // these variable makes visual effect
+    private float currentDashingDistace;
 
     private void Start()
     {
+        currentDashingDistace = dashingDistace;
         movement = GetComponent<PlayerMovement>();
     }
-    /*
+    
     void Update()
     {
-        if (isDashing)
-        {
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) // INPUT
-        {
-            StartCoroutine(DashAction());
-        }
+        Flip();
+        //DashWithKeyboard();
     }
-    */
+    
     private void FixedUpdate()
     {
         if (isDashing)
@@ -41,53 +38,69 @@ public class FinalDash : MonoBehaviour
         velocity = new Vector3(0, velocity.y, velocity.z);
     }
     float temp;
+    private void DashWithKeyboard()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) // INPUT
+        {
+            StartCoroutine(DashAction());
+        }
+    }
+    public void DashWithJoystick(InputAction.CallbackContext context)
+    {
+        if (canDash)
+        {
+            StartCoroutine(DashAction());
+        }
+    }
 
     private IEnumerator DashAction()
     {
-        CheckForCollision(); 
+        CheckForCollision();
         canDash = false;
         isDashing = true;
         temp = movement.GetDownwardForce();
         if (stopGravityWhileDashing) movement.SetDownwardForce(0);
-        if(movement.velocity.x > 0.1)
+        if (movement.velocity.x > 0.1)
         {
-            velocity = new Vector3(dashingDistace - movement.velocity.x, 0f, 0f);
+            velocity = new Vector3(currentDashingDistace - movement.velocity.x, 0f, 0f);
         }
         else if (movement.velocity.x < 0.1)
         {
-            velocity = new Vector3(-dashingDistace - movement.velocity.x, 0f, 0f);
+            velocity = new Vector3(-currentDashingDistace - movement.velocity.x, 0f, 0f);
         }
-        else
-        {
-            velocity = new Vector3(0f, 0f, 0f);
-            Debug.Log("Fuck you");
-        }
-        
+
         //tr.emitting = true; //See variable TrailRenderer tr
         yield return new WaitForSeconds(dashingDuration);
         //tr.emitting = false; //See variable TrailRenderer tr
+        currentDashingDistace = dashingDistace;
         movement.SetDownwardForce(temp);
         isDashing = false;
         yield return new WaitForSeconds(dashingActivationCooldown);
         canDash = true;
 
     }
+    private void Flip()
+    {
+        if (isFacingRight && movement.velocity.x < 0f || !isFacingRight && movement.velocity.x > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
 
     private void CheckForCollision()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, movement.velocity, out hit, 5f, movement.WallLayer))
+        if (Physics.Raycast(transform.position, movement.velocity, out hit, 5f, movement.WallLayer))
         {
-            dashingDistace = hit.distance - 0.5f; /// 0.5f är spelarens storlek
+            currentDashingDistace = hit.distance - 0.5f; /// 0.5f är spelarens storlek
 
-        }
-    }
-
-    public void DoDash(InputAction.CallbackContext context)
-    {
-        if (canDash)
-        {
-            StartCoroutine(DashAction());
         }
     }
 
