9fca41d35d52f147b38b81466368a0af4366bc97
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-10-19 17:05:08 +0200

    weaponspawen fix

--- Assets/Scripts/Weapon/WeaponSpawnerManager.cs
+++ Assets/Scripts/Weapon/WeaponSpawnerManager.cs
@@ -4,42 +4,47 @@ using UnityEngine;
 
 public class WeaponSpawnerManager : MonoBehaviour
 {
+    [SerializeField] private float initialSpawnWeaponsTimer;
     [SerializeField] private float spawnWeaponsTimer;
     [SerializeField] private float numberOfWeaponsToSpawn;
+    private float currentNumberOfWeaponsToSpawn;
     [SerializeField] private WeaponSpawner[] spawners;
     private List<WeaponSpawner> choosenSpawners = new List<WeaponSpawner>();
+    private bool isReadyToSpawn = false;
 
     // Start is called before the first frame update
     void Start()
     {
         ControlNumberOfWeaponToSpawners();
-        StartCoroutine(SpawnWeapons());
+
+        StartCoroutine(InitialSpawnWeapons());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isReadyToSpawn) StartCoroutine(SpawnWeapons());
     }
     private void ChooseRandomSpawners()
     {
         int temporaryNumber;
-        for(int i = 0; i < numberOfWeaponsToSpawn; i++)
+        for(int i = 0; i < currentNumberOfWeaponsToSpawn && i < spawners.Length; i++)
         {
             temporaryNumber = Random.Range(0, spawners.Length);
             if (spawners[temporaryNumber].GetIsHoldingWeapon() == true)
             {
-                i--;
+                currentNumberOfWeaponsToSpawn++;
             }
             else
             {
                 choosenSpawners.Add(spawners[temporaryNumber]);
+                currentNumberOfWeaponsToSpawn = numberOfWeaponsToSpawn;
             }
         }
     }
     private void SpawnWeaponsInSpawners()
     {
-        foreach(var spawner in spawners)
+        foreach(var spawner in choosenSpawners)
         {
             spawner.SpawnRandomWeapon();
         }
@@ -52,12 +57,23 @@ public class WeaponSpawnerManager : MonoBehaviour
         choosenSpawners.Clear();
         StartCoroutine(SpawnWeapons());
     }
+    IEnumerator InitialSpawnWeapons()
+    {
+        yield return new WaitForSeconds(initialSpawnWeaponsTimer);
+        ChooseRandomSpawners();
+        SpawnWeaponsInSpawners();
+        choosenSpawners.Clear();
+        StartCoroutine(SpawnWeapons());
+    }
+
     private void ControlNumberOfWeaponToSpawners()
     {
+
         if(numberOfWeaponsToSpawn > spawners.Length)
         {
             numberOfWeaponsToSpawn = spawners.Length;
             Debug.LogError("\"numberOfWeaponsToSpawn\" is larger than the number of spawners that exists in the Scene");
         }
+        currentNumberOfWeaponsToSpawn = numberOfWeaponsToSpawn;
     }
 }
