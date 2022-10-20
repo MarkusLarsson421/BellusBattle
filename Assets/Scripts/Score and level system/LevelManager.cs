6868e548ef1233b74b76ba834e4e645bbe27c10f
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-10-20 17:05:27 +0200

    Levelmanager update + lagt till weapon prefabs i WeaponSpawner

--- Assets/Scripts/Score and level system/LevelManager.cs
+++ Assets/Scripts/Score and level system/LevelManager.cs
@@ -1,21 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    private enum WhichScenesListToPlay{ ScenesFromBuild, ScencesFromList };
+    [SerializeField] WhichScenesListToPlay scenceToPlay;
     private int sceneCount;
-    private string[] scenes;
     [SerializeField] private float timer = 5;
+    [SerializeField] private string[] scenes;
+    [SerializeField] private string[] scenesToRemove;
+    private enum WhichOrderToPlayScenes { Random, NumiricalOrder };
+    [SerializeField] WhichOrderToPlayScenes playingScenesOrder;
+    private List<string> scenesToChooseFrom = new List<string>();
     private float temp;
     void Start()
     {
-        
-        CreateListOfScenes();
+        if (scenceToPlay == WhichScenesListToPlay.ScenesFromBuild) CreateListOfScenesFromBuild();
+        else if (scenceToPlay == WhichScenesListToPlay.ScencesFromList) CreateListOfScenesFromList();
+        CreateListTofScenesToChooseFrom();
     }
-
-    [System.Obsolete]
+    private void LoadNextScene()
+    {
+        if (playingScenesOrder == WhichOrderToPlayScenes.Random) LoadNextSceneInRandomOrder();
+        else if (playingScenesOrder == WhichOrderToPlayScenes.NumiricalOrder) LoadNextSceneInNumericalOrder();
+    }
+    private void CreateListOfScenesFromBuild()
+    {
+        sceneCount = SceneManager.sceneCountInBuildSettings;
+        for (int i = 0; i < sceneCount - 1; i++)
+        {
+            scenesToChooseFrom[i] = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+        }
+        if (scenesToChooseFrom.Count >= 0) Debug.LogError("There is no scenes in build. please put scenes in build or choose ScencesFromList from " + gameObject);
+    }
+    private void CreateListOfScenesFromList()
+    {
+        foreach (string scene in scenes)
+        {
+            scenesToChooseFrom.Add(scene);
+        }
+        if (scenesToChooseFrom.Count >= 0) Debug.LogError("There is no scenes in build. please put scenes in scences list or choose ScenesFromBuild from " + gameObject);
+    }
+    private void CreateListTofScenesToChooseFrom()
+    {
+        scenesToChooseFrom.Remove("MainMenu");
+    }
     void Update()
     {
         
@@ -23,6 +56,7 @@ public class LevelManager : MonoBehaviour
 
     public void StartNewLevel()
     {
+        LoadNextScene();
         /*
         int randomNumber;
         string curentScene = scenes[0]; // Main menu scene
@@ -34,20 +68,15 @@ public class LevelManager : MonoBehaviour
         SceneManager.LoadScene(scenes[randomNumber]);
         temp = 0;
         */
-        int index = Random.Range(1, sceneCount);
-        SceneManager.LoadScene(index);
-        
+
     }
-    private void CreateListOfScenes()
+    private void LoadNextSceneInNumericalOrder()
     {
-        sceneCount = SceneManager.sceneCountInBuildSettings;
-        scenes = new string[sceneCount];
-        for (int i = 0; i < sceneCount-1; i++)
-        {
-            scenes[i] = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
-            Debug.Log(System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i)));
-        }
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    private void LoadNextSceneInRandomOrder()
+    {
+        SceneManager.LoadScene(scenesToChooseFrom.ElementAt(Random.Range(1, scenesToChooseFrom.Count)));
     }
 
 }
