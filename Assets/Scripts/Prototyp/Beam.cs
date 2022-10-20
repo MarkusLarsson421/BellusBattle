d82528b36a054476d480965f47f7b396c4a8c014
Razerclaws <slutbacktim@gmail.com>
2022-10-20 18:56:25 +0200

    Xgrenade

--- Assets/Scripts/Prototyp/Beam.cs
+++ Assets/Scripts/Prototyp/Beam.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Beam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Gun;
+
+    public GameObject Bullet;
+    public GameObject Beamy;
+
+    public float bulletSpeed;
+
+    public Transform ShootPoint;
+
+    public float fireRate;
+
+    public Camera mainCamera;
+
+
+
+    public float timeUntilBeam = 1.7f;
+
+    public float ammo = 6;
+
+
+    GameObject beamIns;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeUntilBeam <= 0)
+        {
+            beamIns = Instantiate(Beamy, ShootPoint.position, ShootPoint.rotation);
+            beamIns.transform.Rotate(Vector3.left * 180);
+            Destroy(beamIns, 0.5f);
+            timeUntilBeam = 1.7f;
+        }
     }
 }
