a371db67bd7d644e264faf01c6f953315f59c1c0
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2022-11-03 11:07:40 +0100

    door progress

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private GameObject anchor;
-    [SerializeField] private int playerLayer;
-    [SerializeField] private Collider[] colliders; 
+    [SerializeField] private Collider[] colliders;
+    [SerializeField] private int doorHealth;
+    private List<Collider> colliderList = new List<Collider>();
     private BoxCollider boxCollider;
     private GameObject currentPlayer;
+
+
+    public int DoorHealth
+    {
+        get => doorHealth;
+        set => doorHealth = value;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +27,15 @@ public class Door : MonoBehaviour
     private void FixedUpdate()
     {
         CheckForPlayers();
+        colliderList = new List<Collider>(colliders);
+        if (!colliderList.Contains(currentPlayer.GetComponent<Collider>()))
+        {
+            currentPlayer = null;
+        }
+        if(doorHealth <= 0)
+        {
+            DestroyDoor();
+        }
     }
 
     
@@ -28,7 +45,7 @@ public class Door : MonoBehaviour
         colliders = Physics.OverlapBox(transform.position, transform.localScale * 2, Quaternion.identity);
         foreach(Collider col in colliders)
         {
-            if (col.CompareTag("Player"))
+            if (col.CompareTag("Player") && col.gameObject != currentPlayer && currentPlayer == null)
             {
                 currentPlayer = col.gameObject;
                 if(col.bounds.center.x > boxCollider.bounds.center.x)
@@ -41,7 +58,7 @@ public class Door : MonoBehaviour
                 }
                 return;
             }
-            else
+            else if( currentPlayer == null)
             {
                 anchor.transform.localEulerAngles = Vector3.zero;
             }
@@ -53,5 +70,10 @@ public class Door : MonoBehaviour
     {
         if ((anchor.transform.localEulerAngles.y == 90f && rotationAmount > 0f) || (anchor.transform.localEulerAngles.y == -90f && rotationAmount < 0f)) return;
         anchor.transform.Rotate(new Vector3(0f,rotationAmount, 0f));
+    }
+
+    private void DestroyDoor()
+    {
+        gameObject.SetActive(false);
     }
 }
