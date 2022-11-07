019e83adc7208bf30e8d901a1e7e4f492c56b4c7
Simon <simon.h.oscarson@hotmail.se>
2022-11-07 16:26:55 +0100

    AddedSOunds

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,8 +7,8 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private CameraFocus CF;
     public delegate void OnGameOver();
     public static event OnGameOver onGameOver;
-   
-    
+    [SerializeField] private AudioSource playerDeathSound;
+
     private float health = 1;
     private bool isInvinsable=false;
 
@@ -29,6 +29,7 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             KillPlayer();
+            playerDeathSound.Play();
             //onGameOver.Invoke();
         }
     }
