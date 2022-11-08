29c328acef6f336d49501a374591535f54ed9750
khalosh74 <alrassk74@gmail.com>
2022-11-08 18:41:10 +0100

    audio settings text fix

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer myAudioMixer;
+    [SerializeField] private TextMeshProUGUI valumeValue;
+    private void Start()
+    {
+        valumeValue.text = "10"; // place holder
+    }
 
     public void SetValume(float sliderValue)
     {
         myAudioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        valumeValue.text = Mathf.Round(sliderValue * 10).ToString();
     }
 }
