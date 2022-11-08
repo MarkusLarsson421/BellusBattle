df7dd9a32bdfb7b6758767d9f8ab1c6796c6873c
khalosh74 <alrassk74@gmail.com>
2022-11-08 14:38:50 +0100

    Fixat knappar till menu settings

---
+++ Assets/Scripts/UI/UIMenuHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuHandler : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> panels = new List<GameObject>();
+    private GameObject activePanel;
+    private void Start()
+    {
+        panels[0].SetActive(true);
+        activePanel = panels[0];
+    }
+    public void SetPanelActive(GameObject panel)
+    {
+        panel.SetActive(true);
+        activePanel.SetActive(false);
+        activePanel = panel;
+    }
+    public void OpenTheSuprise()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+}
