7aac26f63e2dd21968bb16896f2c5360fcbc8e5c
khalosh74 <alrassk74@gmail.com>
2022-11-08 18:10:03 +0100

    audio mixer

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AudioMixer myAudioMixer;
 
-    // Update is called once per frame
-    void Update()
+    public void SetValume(float sliderValue)
     {
-        
+        myAudioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
     }
 }
