251bdf065ad6644dcd86ee4d398f8bdc2cc6667d
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-11-09 15:47:15 +0100

    level material change test

--- Assets/Scripts/UI/BackgroundMaterialHolder.cs
+++ Assets/Scripts/UI/BackgroundMaterialHolder.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BackgroundMaterialHolder : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> objMaterials = new List<GameObject>();
-    [SerializeField] private Color newColor;
-    // Start is called before the first frame update
+    [Header("Level 1")]
+    [SerializeField] private List<MeshRenderer> M01 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M11 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M21 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M31 = new List<MeshRenderer>();
+    [SerializeField] private List<Material> darkenMaterial1 = new List<Material>();
+    [Header("Level 2")]
+    [SerializeField] private List<MeshRenderer> M02 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M12 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M22 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M32 = new List<MeshRenderer>();
+    [SerializeField] private List<Material> darkenMaterial2 = new List<Material>();
+    [Header("Level 3")]
+    [SerializeField] private List<MeshRenderer> M03 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M13 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M23 = new List<MeshRenderer>();
+    [SerializeField] private List<MeshRenderer> M33 = new List<MeshRenderer>();
+    [SerializeField] private List<Material> darkenMaterial3 = new List<Material>();
+
+
     void Start()
     {
-        objMaterials[0].GetComponent<MeshRenderer>().material.color = newColor;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for(int i = 0; i < M02.Count; i++)
+        {
+            M02[i].material = darkenMaterial2[0];
+        }
+        for (int i = 0; i < M12.Count; i++)
+        {
+            M12[i].material = darkenMaterial2[1];
+        }
+        for (int i = 0; i < M22.Count; i++)
+        {
+            M22[i].material = darkenMaterial2[2];
+        }
+        for (int i = 0; i < M32.Count; i++)
+        {
+            M32[i].material = darkenMaterial2[3];
+        }
     }
 }
