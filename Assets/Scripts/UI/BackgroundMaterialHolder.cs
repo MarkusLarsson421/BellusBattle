9304fa7bc6b023b11dd3981f06a81e5b1881127c
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-11-09 16:14:24 +0100

    .

--- Assets/Scripts/UI/BackgroundMaterialHolder.cs
+++ Assets/Scripts/UI/BackgroundMaterialHolder.cs
@@ -8,8 +8,6 @@ public class BackgroundMaterialHolder : MonoBehaviour
     [Header("Level 3")]
     [SerializeField] private List<MeshRenderer> M01 = new List<MeshRenderer>();
     [SerializeField] private List<MeshRenderer> M11 = new List<MeshRenderer>();
-    [SerializeField] private List<MeshRenderer> M21 = new List<MeshRenderer>();
-    [SerializeField] private List<MeshRenderer> M31 = new List<MeshRenderer>();
     [SerializeField] private List<Material> darkenMaterial1 = new List<Material>();
     [Header("Level 1")]
     [SerializeField] private List<MeshRenderer> M02 = new List<MeshRenderer>();
@@ -26,34 +24,53 @@ public class BackgroundMaterialHolder : MonoBehaviour
 
     void Start()
     {
-        //for(int i = 0; i < M02.Count; i++)
-        //{
-        //    M02[i].material = darkenMaterial2[0];
-        //}
-        //for (int i = 0; i < M12.Count; i++)
-        //{
-        //    M12[i].material = darkenMaterial2[1];
-        //}
-        //for (int i = 0; i < M22.Count; i++)
-        //{
-        //    M22[i].material = darkenMaterial2[2];
-        //}
-        //for (int i = 0; i < M32.Count; i++)
-        //{
-        //    M32[i].material = darkenMaterial2[3];
-        //}
-
-        //for (int i = 0; i < M03.Count; i++)
-        //{
-        //    M03[i].material = darkenMaterial3[0];
-        //}
-        //for (int i = 0; i < M13.Count; i++)
-        //{
-        //    M13[i].material = darkenMaterial3[1];
-        //}
-        //for (int i = 0; i < M23.Count; i++)
-        //{
-        //    M23[i].material = darkenMaterial3[2];
-        //}
+        LVL1();
+        LVL2();
+        LVL3();
+    }
+    private void LVL1()
+    {
+        for (int i = 0; i < M02.Count; i++)
+        {
+            M02[i].material = darkenMaterial2[0];
+        }
+        for (int i = 0; i < M12.Count; i++)
+        {
+            M12[i].material = darkenMaterial2[1];
+        }
+        for (int i = 0; i < M22.Count; i++)
+        {
+            M22[i].material = darkenMaterial2[2];
+        }
+        for (int i = 0; i < M32.Count; i++)
+        {
+            M32[i].material = darkenMaterial2[3];
+        }
+    }
+    private void LVL2()
+    {
+        for (int i = 0; i < M03.Count; i++)
+        {
+            M03[i].material = darkenMaterial3[0];
+        }
+        for (int i = 0; i < M13.Count; i++)
+        {
+            M13[i].material = darkenMaterial3[1];
+        }
+        for (int i = 0; i < M23.Count; i++)
+        {
+            M23[i].material = darkenMaterial3[2];
+        }
+    }
+    private void LVL3()
+    {
+        for (int i = 0; i < M01.Count; i++)
+        {
+            M01[i].material = darkenMaterial1[0];
+        }
+        for (int i = 0; i < M11.Count; i++)
+        {
+            M11[i].material = darkenMaterial1[1];
+        }
     }
 }
