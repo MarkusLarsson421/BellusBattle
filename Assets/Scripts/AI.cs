bf54d25c4301a4ad4f1b98179b0c8ca5ad04f4eb
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-11-09 17:02:26 +0100

    .

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckCollision();
+    }
+    private void FixedUpdate()
+    {
+        velocity = direction * speed * Time.deltaTime;
+        transform.position += velocity;
+    }
+    private void CheckCollision()
+    {
+        if (Physics.BoxCast(transform.position, transform.localScale, direction, Quaternion.identity, speed/2))
+        {
+            if (direction == Vector3.right) direction = Vector2.left;
+            else direction = Vector2.right;
+        }
+    }
+    public void KillAI()
+    {
+        Destroy(gameObject);
+    }
+}
